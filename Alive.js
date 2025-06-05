98826bbf554d4ef84e678d228a3188692fe42bfa
Allan256prog <richardbyaruhanga59@gmail.com>
2025-06-05 18:14:07 +0300

    Update Alive.js

--- Alive.js
+++ Alive.js
@@ -1,7 +1,7 @@
 import cron from 'node-cron';
 import axios from 'axios';
 
-const url = 'https://allanpro.onrender.com';
+const url = https://allanpro-1.onrender.com';
 
 cron.schedule('*/14 * * * *', async () => {
   try {
